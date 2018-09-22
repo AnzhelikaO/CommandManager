e602a8dce06cb1744c5cc1b6a76acff35348fc52
AnzhelikaO <qwerty5867192@gmail.com>
2018-09-22 11:14:30 +0900

    Made it. Wow. It works.

---
+++ CommandManager/Attributes.cs
@@ -0,0 +1,180 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TShockAPI;
+#endregion
+namespace CommandManager
+{
+    #region Summary
+
+    /// <summary> Adding this attrubute to method means
+    /// that it is a CommandDelegate of a command.
+    /// <para> Add <see cref="Names"/> attribute
+    /// to define <see cref="Command.Names"/>. </para>
+    /// Add <see cref="Permissions"/> attribute
+    /// to define <see cref="Command.Permissions"/>.
+    /// <para> Add <see cref="Help"/> attribute
+    /// to define <see cref="Command.HelpText"/>.
+    /// Or <see cref="HelpDesc"/> to define
+    /// <see cref="Command.HelpDesc"/>. </para>
+    /// Add <see cref="DisallowServer"/> attribute
+    /// to set <see cref="Command.AllowServer"/> to false.
+    /// <para> Add <see cref="DoNotLog"/> attribute
+    /// to set <see cref="Command.DoLog"/>
+    /// to false. </para> </summary>
+
+    #endregion
+    public class IsCommand : Attribute { }
+    #region Summary
+
+    /// <summary> Will work only with <see cref="IsCommand"/>
+    /// and <see cref="Names"/> attributes.
+    /// <para> Adding this attribute to command delegate
+    /// sets <see cref="Command.AllowServer"/>
+    /// to false. </para> </summary>
+    
+    #endregion
+    public class DisallowServer : Attribute { }
+    #region Summary
+
+    /// <summary> Will work only with <see cref="IsCommand"/>
+    /// and <see cref="Names"/> attributes.
+    /// <para> Adding this attribute to command delegate
+    /// sets <see cref="Command.DoLog"/>
+    /// to false. </para> </summary>
+
+    #endregion
+    public class DoNotLog : Attribute { }
+    #region Summary
+
+    /// <summary> Adding this attribute to command
+    /// delegate means that it will not be added to
+    /// <see cref="Commands.ChatCommands"/> even if 
+    /// <see cref="IsCommand"/> and <see cref="Names"/>
+    /// attributes has been properly added. </summary>
+
+    #endregion
+    public class DoNotRegister : Attribute { }
+    #region Names
+
+    #region Summary
+
+    /// <summary> Will work only with
+    /// <see cref="IsCommand"/> attribute.
+    /// <para> Adding this attribute to command
+    /// delegate defines <see cref="Command.Names"/>. </para>
+    /// Names array and each its element must
+    /// be not null and not empty. </summary>
+    /// <exception cref="InvalidOperationException"></exception>
+
+    #endregion
+    public class Names : Attribute
+    {
+        /// <summary> <see cref="Command.Names"/>. </summary>
+        public string[] CommandNames { get; }
+        #region Summary
+
+        /// <summary> Will work only with
+        /// <see cref="IsCommand"/> attribute.
+        /// <para> Adding this attribute to command
+        /// delegate defines <see cref="Command.Names"/>. </para>
+        /// Names array and each its element must
+        /// be not null and not empty. </summary>
+        /// <exception cref="InvalidOperationException"></exception>
+
+        #endregion
+        public Names(params string[] Names)
+        {
+            string error = "Names array and each its " +
+                "element must be not null and not empty.";
+            this.CommandNames =
+                Names?.Select(n => string.IsNullOrWhiteSpace(n)
+                                    ? throw new InvalidOperationException(error)
+                                    : n.ToLower())?
+                                         .ToArray();
+            if (Names?.Length == 0)
+            { throw new InvalidOperationException(error); }
+        }
+    }
+
+    #endregion
+    #region Permissions
+
+    #region Summary
+
+    /// <summary> Will work only with <see cref="IsCommand"/>
+    /// and <see cref="Names"/> attributes.
+    /// <para> Adding this attribute to command delegate
+    /// defines <see cref="Command.Permissions"/>. </para> </summary>
+
+    #endregion
+    public class Permissions : Attribute
+    {
+        /// <summary> <see cref="Command.Permissions"/>. </summary>
+        public List<string> CommandPermissions { get; }
+        #region Summary
+
+        /// <summary> Will work only with <see cref="IsCommand"/>
+        /// and <see cref="Names"/> attributes.
+        /// <para> Adding this attribute to command delegate
+        /// defines <see cref="Command.Permissions"/>. </para> </summary>
+
+        #endregion
+        public Permissions(params string[] CommandPermissions) =>
+            this.CommandPermissions = (CommandPermissions ?? new string[0]).ToList();
+    }
+
+    #endregion
+    #region Help
+
+    #region Summary
+
+    /// <summary> Will work only with <see cref="IsCommand"/>
+    /// and <see cref="Names"/> attributes.
+    /// <para> Adding this attribute to command delegate
+    /// defines <see cref="Command.HelpText"/>. </para> </summary>
+
+    #endregion
+    public class Help : Attribute
+    {
+        /// <summary> <see cref="Command.HelpText"/>. </summary>
+        public string CommandHelp { get; }
+        #region Summary
+
+        /// <summary> Will work only with <see cref="IsCommand"/>
+        /// and <see cref="Names"/> attributes.
+        /// <para> Adding this attribute to command delegate
+        /// defines <see cref="Command.HelpText"/>. </para> </summary>
+
+        #endregion
+        public Help(string CommandHelp) =>
+            this.CommandHelp = (CommandHelp ?? "No help available.");
+    }
+
+    #region Summary
+
+    /// <summary> Will work only with <see cref="IsCommand"/>
+    /// and <see cref="Names"/> attributes.
+    /// <para> Adding this attribute to command delegate
+    /// defines <see cref="Command.HelpDesc"/>. </para> </summary>
+
+    #endregion
+    public class HelpDesc : Attribute
+    {
+        /// <summary> <see cref="Command.HelpDesc"/>. </summary>
+        public string[] CommandHelpDesc { get; }
+        #region Summary
+
+        /// <summary> Will work only with <see cref="IsCommand"/>
+        /// and <see cref="Names"/> attributes.
+        /// <para> Adding this attribute to command delegate
+        /// defines <see cref="Command.HelpDesc"/>. </para> </summary>
+
+        #endregion
+        public HelpDesc(params string[] CommandHelpDesc) =>
+            this.CommandHelpDesc = CommandHelpDesc;
+    }
+
+    #endregion
+}
---
+++ CommandManager/CommandManager.cs
@@ -0,0 +1,120 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using TerrariaApi.Server;
+using TShockAPI;
+#endregion
+namespace CommandManager
+{
+    /// <summary> Load/unload commands. </summary>
+    public class CommandManager
+    {
+        #region GetAllCommandDelegates
+
+        #region Summary
+
+        /// <summary> Gets all methods of Plugin's assembly
+        /// with <see cref="IsCommand"/> and <see cref="Names"/>
+        /// attributes and without <see cref="DoNotRegister"/>
+        /// attribute that matches Flags and are type of
+        /// <see cref="CommandDelegate"/>. </summary>
+        
+        #endregion
+        public static List<MethodInfo> GetAllCommandDelegates(TerrariaPlugin Plugin)
+        {
+            List<MethodInfo> cmds = new List<MethodInfo>();
+            foreach (Type type in Assembly.GetAssembly(Plugin.GetType()).GetTypes())
+            {
+                foreach (MethodInfo method in type.GetMethods
+                    (BindingFlags.Public | BindingFlags.Static))
+                {
+                    ParameterInfo[] @params = method?.GetParameters();
+                    if ((@params?.Length != 1)
+                        || (@params[0].ParameterType != typeof(CommandArgs))
+                        || (method.ReturnType != typeof(void)))
+                    { continue; }
+                    IEnumerable<Attribute> attributes = method.GetCustomAttributes();
+                    if (attributes.Any(a => (a.GetType() == typeof(DoNotRegister)))
+                        || !attributes.Any(a => a.GetType() == typeof(IsCommand))
+                        || !attributes.Any(a => a.GetType() == typeof(Names)))
+                    { continue; }
+                    cmds.Add(method);
+                }
+            }
+            return cmds;
+        }
+
+        #endregion
+        #region LoadAll
+
+        /// <summary> Registers all methods found in
+        /// <see cref="GetAllCommandDelegates(TerrariaPlugin)"/>.
+        /// </summary>
+        public static void LoadAll(TerrariaPlugin Plugin)
+        {
+            foreach (MethodInfo method in GetAllCommandDelegates(Plugin))
+            {
+                List<Attribute> attributes = method.GetCustomAttributes().ToList();
+                Permissions permissions = (Permissions)attributes.FirstOrDefault
+                    (a => (a is Permissions)) ?? new Permissions();
+                CommandByManager cmd = new CommandByManager
+                    (Plugin.GetType(), permissions.CommandPermissions,
+                    (CommandDelegate)Delegate.CreateDelegate
+                    (typeof(CommandDelegate), method),
+                    method.GetCustomAttribute<Names>().CommandNames);
+                foreach (Attribute a in attributes)
+                {
+                    if (a is Help h)
+                    { cmd.HelpText = h.CommandHelp; }
+                    else if (a is HelpDesc d)
+                    { cmd.HelpDesc = d.CommandHelpDesc; }
+                    else if (a is DisallowServer)
+                    { cmd.AllowServer = false; }
+                    else if (a is DoNotLog)
+                    { cmd.DoLog = false; }
+                }
+                Console.WriteLine(cmd.CommandDelegate?.Method?.Name);
+                Console.WriteLine(cmd.Name);
+                Console.WriteLine(cmd.Permissions.FirstOrDefault());
+                Commands.ChatCommands.Add(cmd);
+                Console.WriteLine(Commands.ChatCommands.Last().CommandDelegate?.Method?.Name);
+                Console.WriteLine(Commands.ChatCommands.Last().Name);
+                Console.WriteLine(Commands.ChatCommands.Last().Permissions.FirstOrDefault());
+            }
+        }
+
+        #endregion
+        #region UnloadAll
+
+        /// <summary> Deletes all commands that were registered
+        /// by <see cref="LoadAll(TerrariaPlugin)"/>.
+        /// </summary>
+        public static void UnloadAll(TerrariaPlugin Plugin)
+        {
+            for (int i = Commands.ChatCommands.Count - 1; i >= 0; i--)
+            {
+                Command command = Commands.ChatCommands[i];
+                if ((command is CommandByManager cmd)
+                    && (cmd.PluginType == Plugin.GetType()))
+                { Commands.ChatCommands.RemoveAt(i); }
+            }
+        }
+
+        #endregion
+        #region CommandByManager
+
+        internal class CommandByManager : Command
+        {
+            public Type PluginType { get; }
+            public CommandByManager(Type PluginType,
+                List<string> permissions, CommandDelegate cmd,
+                params string[] names)
+                : base(permissions, cmd, names)
+            { this.PluginType = PluginType; }
+        }
+
+        #endregion
+    }
+}
